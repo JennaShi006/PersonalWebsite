142d2a34b36221eb92c7b89741254117b36cb695
Jenna Shi <jennashi0927@gmail.com>
2025-09-06 17:05:16 -0400

    test2

--- frontend/src/Pages/Internship.js
+++ frontend/src/Pages/Internship.js
@@ -5,7 +5,6 @@ function Internship() {
         <div className="Internship">
             <h1>Internship Page</h1>
             <p>This page will contain information about internships.</p>
-            <p>More content</p>
         </div>
     );
 }
